46def0815189d444925040d1a9dd8407d66957b4
RAni <ranmn123@gmail.com>
2022-06-27 15:35:55 +0300

    fixed an error.

--- cost_functions.py
+++ cost_functions.py
@@ -20,7 +20,7 @@ def mse(current_macro_block: np.ndarray, reference_frame_block: np.ndarray, macr
 
     :param current_macro_block: A macro-block from the current frame which is a Numpy array containing RGB triples.
     :param reference_frame_block: A macro-block from the reference frame which is a Numpy array containing RGB triples.
-    :param macro_block_size: An int representing the size (width/high) of the macro-blocks.
+    :param macro_block_size: An int representing the size (width/height) of the macro-blocks.
     :return: A float used as a metric for evaluating a macro-block with another macro-block.
     """
     differences = np.square(current_macro_block - reference_frame_block)
