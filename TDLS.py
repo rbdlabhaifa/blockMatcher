f7e39ad3325832c9f4d7c6d9d092cd8206cec477
Amir <amir1132004@gmail.com>
2022-06-27 15:34:39 +0300

    added sliced macroblock function and interface for TDLS

---
+++ TDLS.py
@@ -0,0 +1,10 @@
+def TDLS(reference_frame, mc, block_size, step):
+    """
+    :param reference_frame: of type numpy ndarray in BGR format
+    :param mc: ndarray containing the top left coordinates of the macroblock to search for, formated as [x, y]
+    :param block_size: size of block in pixels, must be a power of a whole number
+    :param step: search step size of type int
+    :return: returns absolute coordinates of the center of the block in pixels
+    """
+
+    pass
