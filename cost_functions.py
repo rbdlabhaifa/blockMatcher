41fb3fdb267dbb05167822e2e92d7e3636623090
RAni <ranmn123@gmail.com>
2022-06-27 14:54:13 +0300

    added two files used for all cost functions and block matching fuctions

---
+++ cost_functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def mad(i_frame_block: np.ndarray, current_frame_block: np.ndarray, macro_block_size: int):
+    """
+    Mean Absolute Difference (MAD).
+
+    :param i_frame_block: A macro-block from the i-frame which is a Numpy array containing RGB tuples.
+    :param current_frame_block: A macro-block from the current frame which is a Numpy array containing RGB tuples.
+    :param macro_block_size: An int representing the size of the macro-block.
+    :return:
+    """
+    pass
+
+
+def mse():
+    """
+    Mean Squared Error (MSE).
+
+    :return:
+    """
+    pass
