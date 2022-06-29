a4a9c53c3cf436e2ba10f85a4ee617d86dea49a7
BenGobi1 <bengobi1@gmail.com>
2022-06-29 20:41:33 +0300

    Added test function and a function to grayscale an image.

--- test2.py
+++
@@ -1,6 +0,0 @@
-from python_block_matching.utils import *
-from python_block_matching.cost_functions import *
-from python_block_matching.algorithms import *
-
-
-test_algorithm('vid.mp4', three_step_search)
