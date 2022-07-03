bdf344b85950f704f66aec9a1c25089f71a7ad73
Amir <amir1132004@gmail.com>
2022-07-03 12:00:43 +0300

    deleted ds.py and added SSE cost function

--- python_block_matching/cost_functions.py
+++ python_block_matching/cost_functions.py
@@ -23,3 +23,15 @@ def mse(current_macro_block: np.ndarray, reference_frame_block: np.ndarray) -> f
     """
     block_area = current_macro_block.shape[0] * current_macro_block.shape[1]
     return np.sum(np.square(np.subtract(current_macro_block, reference_frame_block))) / block_area
+
+
+def sse(current_macro_block: np.ndarray, reference_frame_block: np.ndarray) -> float:
+    """
+    Sum of Squared Errors (SSE).
+
+    :param current_macro_block: A macro-block from the current frame which is a Numpy array containing RGB triples.
+    :param reference_frame_block: A macro-block from the reference frame which is a Numpy array containing RGB triples.
+    :return: A float used as a metric for evaluating a macro-block with another macro-block.
+    """
+    block_area = current_macro_block.shape[0] * current_macro_block.shape[1]
+    return np.sum(np.square(np.subtract(current_macro_block, reference_frame_block)))
