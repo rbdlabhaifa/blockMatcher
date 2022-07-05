1c4a56523760c883571ec47ddb84218bc1550df4
BenGobi1 <bengobi1@gmail.com>
2022-07-05 15:20:25 +0300

    Added a cost function and tests for block partitioning.

--- python_block_matching/cost_functions.py
+++ python_block_matching/cost_functions.py
@@ -35,3 +35,14 @@ def sse(current_macro_block: np.ndarray, reference_frame_block: np.ndarray) -> f
     """
     block_area = current_macro_block.shape[0] * current_macro_block.shape[1]
     return np.sum(np.square(np.subtract(current_macro_block, reference_frame_block)))
+
+
+def sad(current_macro_block: np.ndarray, reference_frame_block: np.ndarray) -> float:
+    """
+    Sum of Absolute Difference (SAD).
+
+    :param current_macro_block: A macro-block from the current frame which is a Numpy array containing RGB triples.
+    :param reference_frame_block: A macro-block from the reference frame which is a Numpy array containing RGB triples.
+    :return: A float used as a metric for evaluating a macro-block with another macro-block.
+    """
+    return np.sum(np.abs(np.subtract(current_macro_block, reference_frame_block)))
