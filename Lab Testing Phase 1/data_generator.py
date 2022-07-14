c8c2237383ae781dea475d89c2cdd54c8535d7a8
Amir <amir1132004@gmail.com>
2022-07-14 09:37:22 +0300

    Renamed generate_movement to generate_translation and added a function to generate frames of rotation called generate_rotation

--- Lab Testing Phase 1/data_generator.py
+++ Lab Testing Phase 1/data_generator.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
-from typing import List
+from typing import Tuple, Union, List, Dict
 from PIL import Image
+
+from python_block_matching import BMFrame
 
 
 class DataGenerator:
     @staticmethod
-    def generate_movement(frame_size: List[int], img_url: str, translation):
+    def generate_translation(frame_size: List[int], img_url: str, translation):
         """
         @param frame_size: [width, height] of overall frame, must be bigger than the pasted image's size
         @param img_url: url of the image to be pasted.
         @param translation: [translation in the x-axis, translation in the y-axis]
-        @return:
+        @return: The frame before the motion (Reference), The frame after the motion (Current)
         """
         frame_size.append(frame_size.pop(0))
         frame_size.append(3)
@@ -29,10 +31,35 @@ class DataGenerator:
 
         return np.asarray(ref_frame), np.asarray(cur_frame)
 
+    @staticmethod
+    def generate_rotation(frame_size: List[int], img_url: str, angle: Union[int, float]):
+        """
+        @param frame_size: [width, height] of overall frame, must be bigger than the pasted image's size
+        @param img_url: url of the image to be pasted.
+        @param angle: angle of rotation in degrees. positive = clockwise, negative = counter-clockwise
+        @return: The frame before the motion (Reference), The frame after the motion (Current)
+        """
+        frame_size.append(frame_size.pop(0))
+        frame_size.append(3)
+        ref_frame = Image.fromarray(np.full(frame_size, 255, dtype=np.uint8), 'RGB')
+        cur_frame = ref_frame.copy()
+        img = Image.open(img_url)
+        assert ref_frame.size[0] > img.size[0] and ref_frame.size[1] > img.size[1]
+        ref_frame.paste(img, (ref_frame.size[0] // 2 - img.size[0] // 2, ref_frame.size[1] // 2 - img.size[1] // 2,
+                              ref_frame.size[0] // 2 + img.size[0] // 2, ref_frame.size[1] // 2 + img.size[1] // 2))
+        img = img.rotate(angle, expand=True, fillcolor=(255,255,255))
+        cur_frame.paste(img, (ref_frame.size[0] // 2 - img.size[0] // 2, ref_frame.size[1] // 2 - img.size[1] // 2,
+                              ref_frame.size[0] // 2 + img.size[0] // 2, ref_frame.size[1] // 2 + img.size[1] // 2))
+
+        return np.asarray(ref_frame), np.asarray(cur_frame)
+
+
 if __name__ == '__main__':
-    # for i in range(15):
-    arr = np.full((10,10,3),[0,0,0], dtype=np.uint8)
-    # arr = np.random.randint(0, 256, (10, 10, 3), dtype=np.uint8)
-    arr = cv2.resize(arr, (160, 160), interpolation=cv2.INTER_AREA)
-    img = Image.fromarray(arr, "RGB")
-    img.save("/home/txp2/RPI-BMA-RE/Lab Testing Phase 1/Benchmark_Pictures/Black.png")
+    ref, cur = DataGenerator.generate_rotation([360, 360],
+                                               "/home/txp2/RPI-BMA-RE/Lab Testing Phase 1/Benchmark_Pictures/Image0.png",
+                                               20)
+    ref = BMFrame(ref)
+    cur = BMFrame(cur)
+    ref.show()
+    cur.show()
+    cv2.destroyAllWindows()
