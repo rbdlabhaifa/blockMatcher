bdca220c1ecc53066a8754f5ea0e3aa3c8da9011
Amir <amir1132004@gmail.com>
2022-07-19 13:34:19 +0300

    Added gradient to data generation and transparency to rotation

--- Dictionary Testing/data_generator.py
+++ Dictionary Testing/data_generator.py
@@ -8,6 +8,13 @@ from python_block_matching import BMFrame, BlockMatching
 class DataGenerator:
 
     @staticmethod
+    def get_gradient_2d(start, stop, width, height, is_horizontal):
+        if is_horizontal:
+            return np.tile(np.linspace(start, stop, width), (height, 1))
+        else:
+            return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+    @staticmethod
     def generate_translation(frame_size: List[int], img_url: str, translation):
         """
         @param frame_size: [width, height] of overall frame, must be bigger than the pasted image's size
@@ -17,11 +24,12 @@ class DataGenerator:
         """
         frame_size = frame_size.copy()
         frame_size.append(frame_size.pop(0))
-        frame_size.append(3)
-        ref_frame = Image.fromarray(np.full(frame_size, 255, dtype=np.uint8), 'RGB')
+        # frame_size.append(3)
+        ref_frame = Image.open("synthetic data/gradient.jpeg")
+        ref_frame = ref_frame.resize(frame_size)
         cur_frame = ref_frame.copy()
         img = Image.open(img_url)
-        assert ref_frame.size[0] > img.size[0] and ref_frame.size[1] > img.size[1]
+        # assert ref_frame.size[0] > img.size[0] and ref_frame.size[1] > img.size[1]
         ref_frame.paste(img, (ref_frame.size[0] // 2 - img.size[0] // 2, ref_frame.size[1] // 2 - img.size[1] // 2,
                               ref_frame.size[0] // 2 + img.size[0] // 2, ref_frame.size[1] // 2 + img.size[1] // 2))
         cur_frame.paste(img, (
@@ -42,15 +50,24 @@ class DataGenerator:
         """
         frame_size = frame_size.copy()
         frame_size.append(frame_size.pop(0))
-        frame_size.append(3)
-        ref_frame = Image.fromarray(np.full(frame_size, 255, dtype=np.uint8), 'RGB')
+        # frame_size.append(3)
+        ref_frame = Image.open("synthetic data/gradient.jpeg")
+        ref_frame = ref_frame.resize(frame_size)
         cur_frame = ref_frame.copy()
         img = Image.open(img_url)
         assert ref_frame.size[0] > img.size[0] and ref_frame.size[1] > img.size[1]
         ref_frame.paste(img, (ref_frame.size[0] // 2 - img.size[0] // 2, ref_frame.size[1] // 2 - img.size[1] // 2,
                               ref_frame.size[0] // 2 + img.size[0] // 2, ref_frame.size[1] // 2 + img.size[1] // 2))
-        img = img.rotate(-angle, expand=True, fillcolor=(255, 255, 255))
+        img = img.convert("RGBA")
+        img = img.rotate(-angle, expand=True)
         cur_frame.paste(img, (ref_frame.size[0] // 2 - img.size[0] // 2, ref_frame.size[1] // 2 - img.size[1] // 2,
-                              ref_frame.size[0] // 2 + img.size[0] // 2, ref_frame.size[1] // 2 + img.size[1] // 2))
+                              ref_frame.size[0] // 2 + img.size[0] // 2, ref_frame.size[1] // 2 + img.size[1] // 2),
+                        img)
 
         return np.asarray(ref_frame), np.asarray(cur_frame)
+
+
+if __name__ == '__main__':
+    f1, f2 = DataGenerator.generate_rotation([360, 360], "synthetic data/Image0.png", 30)
+    f2 = BMFrame(f2)
+    f2.show()
