f8b817eca2240874b2d48f03d1ce6baaff3324ad
Amir <amir1132004@gmail.com>
2022-07-28 10:58:57 +0300

    Added a function in main.py to test the ego rotation dictionary

--- Dictionary Testing/main.py
+++ Dictionary Testing/main.py
@@ -1,7 +1,25 @@
 import numpy as np
-
+from syntetic import generate_pictures_2_angles
 from python_block_matching import *
 from mv_dictionary import MVMapping
+
+
+def test_ego_rotation_dicts():
+    im_shape = [480, 480]
+    square_size = 32
+    mv_dict = MVMapping('trained dicts/chess_plane_ego_rot_0-8_steps.pickle')
+    while True:
+        angle, step = eval(input('enter angle and step').replace(' ', ','))
+        chess = np.zeros((im_shape[0] * 4, int(im_shape[1] * 4), 3), dtype=np.uint8)
+        for i in range(int(chess.shape[0] / square_size)):
+            for j in range(int(chess.shape[1] / square_size)):
+                chess[square_size * j:square_size * (j + 1), square_size * i:square_size * (i + 1)] = np.random.randint(
+                    0,
+                    256,
+                    3,
+                    np.uint8)
+        f1, f2 = generate_pictures_2_angles(chess, angle, angle + step, im_shape)
+        print('dictionary found: ', mv_dict[BlockMatching.get_motion_vectors(f2, f1)])
 
 
 if __name__ == '__main__':
