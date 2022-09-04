a836ccfe6d490b5e96c95068d273d1e2e5ac0490
RAni <ranmn123@gmail.com>
2022-09-04 19:32:22 +0300

    Finished comparisons, needs to be checked.

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Dict
 from scipy.spatial import KDTree
 
 
@@ -77,6 +77,29 @@ class MVMapping:
                 pickle.dump(self.keys[i], f)
                 pickle.dump(self.values[i], f)
 
-    def compare(self, data_directory: str, output_directory: str) -> None:
-        # TODO:
-        pass
+    def compare(self, data_to_compare: Dict[float, List[Tuple[int, int, int, int]]], output_file: str):
+        """
+        Compares this object with another set of data.
+
+        :param data_to_compare: A dictionary with rotation as keys and vectors as values.
+        :param output_file: The file to save the results of the comparison to.
+        """
+        output = 'real rot, expected dict rot, actual dict rot, error from real rot, error from expected rot\n'
+        total_expected_rot_error = 0
+        total_real_rot_error = 0
+        for real_rotation, motion_vectors in data_to_compare.items():
+            dict_rot = round(self[motion_vectors], 3)
+            real_rotation = round(real_rotation, 3)
+            expected_dict_rot = round(min(self.values, key=lambda x: abs(x - real_rotation)), 3)
+            error_from_real = round(100 * (abs(dict_rot - real_rotation) / real_rotation), 3)
+            error_from_expected = round(100 * (abs(dict_rot - expected_dict_rot) / expected_dict_rot), 3)
+            output += f'{real_rotation}, {expected_dict_rot}, {dict_rot}, {error_from_real}%, {error_from_expected}%\n'
+            total_real_rot_error += error_from_real
+            total_expected_rot_error += error_from_expected
+        total_real_rot_error = round(total_real_rot_error / len(data_to_compare), 3)
+        total_expected_rot_error = round(total_expected_rot_error / len(data_to_compare), 3)
+        output += f'Average error from real rotations: {total_real_rot_error}%\n'
+        output += f'Average error from expected rotations: {total_expected_rot_error}%\n'
+        file = open(output_file, 'w')
+        file.write(output)
+        file.close()
---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+from Dictionary.dictionary import MVMapping
+from block_matching import BlockMatching
+
+
+path = '/home/rani/PycharmProjects/blockMatcher/'
+
+bm = BlockMatching.extract_motion_data(path + 'Extra Code/extract motion data/motionVectors', path + 'Dictionary/data/optitrack/2.mp4')
+d = {}
+for frame in range(len(bm)):
+    if frame % 2 == 1:
+        continue
+    d[0.1 * (frame / 2 + 1)] = bm[frame]
+
+for dictionary in os.listdir(path + 'Dictionary/saved dictionaries/'):
+    if dictionary == 'old dictionaries':
+        continue
+    if not os.path.exists(path + 'Dictionary/comparisons/' + dictionary):
+        os.mkdir(path + 'Dictionary/comparisons/' + dictionary)
+    dic = MVMapping(path + f'Dictionary/saved dictionaries/{dictionary}')
+    dic.compare(d, path + f'Dictionary/comparisons/{dictionary}/compared with optitrack_2.csv')
