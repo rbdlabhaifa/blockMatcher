477bcb365f44767da3b6349e22d47214e7f6fc63
RAni <ranmn123@gmail.com>
2022-09-06 14:48:30 +0300

    Finished drone script.

--- Dictionary/data/create_drone_video.py
+++ Dictionary/data/create_drone_video.py
@@ -1,13 +1,20 @@
+import threading
+
 import cv2
+from typing import List
+import numpy as np
 from djitellopy import Tello
+from threading import Thread
 
 
-def capture(tello: Tello, save_to: str = ''):
-    cv2.imwrite(save_to, tello.get_frame_read().frame)
+def capture(tello: Tello, save_to: str = 'image.png'):
+    frame = tello.get_frame_read().frame
+    cv2.imwrite(save_to, frame)
     print(f'Captured to {save_to} .')
 
 
-def rotate(tello: Tello, theta: int = 0):
+def rotate(tello: Tello, theta: str = '0'):
+    value = int(theta)
     if theta < 0:
         tello.rotate_counter_clockwise(-theta)
         print(f'rotated counter-clockwise by {-theta} degrees.')
@@ -16,35 +23,44 @@ def rotate(tello: Tello, theta: int = 0):
         print(f'rotated clockwise by {theta} degrees.')
 
 
-def up_down(tello: Tello, value: int):
+def up_down(tello: Tello, value: str):
+    value = int(value)
     if value > 0:
         tello.move_up(value)
     else:
         tello.move_down(-value)
 
 
-def forward_backward(tello: Tello, value: int):
+def forward_backward(tello: Tello, value: str):
+    value = int(value)
     if value > 0:
         tello.move_forward(value)
     else:
         tello.move_back(-value)
 
 
-def left_right(tello: Tello, value: int):
+def left_right(tello: Tello, value: str):
+    value = int(value)
     if value > 0:
         tello.move_left(value)
     else:
         tello.move_right(-value)
 
 
-def move(tello: Tello, value: int = 30):
+def move(tello: Tello, value: str = '20'):
+    value = int(value)
+    image_number = 0
     print('You can now control the drone freely with your keyboard.')
     print('Keys: w, a, s, d, e, q, r, f.')
-    print('Press esc to stop.')
+    print('Press esc to stop and space to capture image.')
     while True:
+        cv2.imshow('', tello.get_frame_read().frame)
         key = cv2.waitKey(1) & 0xff
         if key == 27:
             break
+        elif key == ord(' '):
+            capture(tello, f'/home/rani/PycharmProjects/blockMatcher/Dictionary/{image_number}.png')
+            image_number += 1
         elif key == ord('w'):
             tello.move_forward(value)
         elif key == ord('s'):
@@ -65,13 +81,12 @@ def move(tello: Tello, value: int = 30):
 
 def process_command(tello: Tello, command: str):
     allowed_commands = {
-        'capture': capture,
-        'rotate': rotate,
+        'cap': capture,
+        'rot': rotate,
         'up-down': up_down,
         'forward-backward': forward_backward,
         'left-right': left_right,
         'move': move,
-        'record': record
     }
     split_command = ['']
     for i, character in enumerate(command):
@@ -82,11 +97,11 @@ def process_command(tello: Tello, command: str):
                 split_command.append('')
         else:
             split_command[len(split_command) - 1] += character
-    if command[0] not in allowed_commands.keys():
-        print(f'Command {command[0]} is not a valid command!')
+    if split_command[0] not in allowed_commands.keys():
+        print(f'Command {split_command[0]} is not a valid command!')
         print('Allowed commands are:', allowed_commands.keys())
         return
-    allowed_commands[command](tello, *command[1:])
+    threading.Thread(target=allowed_commands[split_command[0]], args=(tello, *split_command[1:])).start()
 
 
 def main():
@@ -95,10 +110,10 @@ def main():
     tello.takeoff()
     tello.streamon()
     while True:
-        command = input('enter command: ').strip().lower()
-        if command == 'stop':
-            break
         try:
+            command = input('enter command: ').strip().lower()
+            if command == 'stop':
+                break
             process_command(tello, command)
         except Exception as e:
             print('Error!')
