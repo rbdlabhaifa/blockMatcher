52601480a92c4943c399d184b64765ce878fe1f7
RAni <ranmn123@gmail.com>
2022-10-19 17:44:40 +0300

    Fixed bugs.

--- main.py
+++ main.py
@@ -32,23 +32,41 @@ def view_vectors(path_to_data, suffix='.png'):
 
 if __name__ == '__main__':
     from mvextractor.videocap import VideoCap
-    vectors_cap = VideoCap()
-    vectors_cap.open('/home/rani/PycharmProjects/data/webcam/2.h264')
 
+    # 640 x 480
+    # camera_matrix = np.array([[647.81841563, 0, 335.8814632],
+    #                           [0, 645.9438274, 225.99776891001],
+    #                           [0, 0, 1]])
+    # 1280 x 720
+    # camera_matrix = np.array([[1374.469951742393, 0, 638.379203340978],
+    #                           [0, 1380.753021831714, 374.04638859868663],
+    #                           [0, 0, 1]])
+    # 960 x 720
+    camera_matrix = np.array([
+        [907.15425425, 0, 478.97775485],
+        [0, 907.55348533, 346.90456115],
+        [0, 0, 1]
+    ])
+    vectors_cap = VideoCap()
+    vectors_cap.open('/home/rani/PycharmProjects/blockMatcher/data/drone/1.mp4')
     was_read, frame, vectors, frame_type, _ = vectors_cap.read()
     i = 0
+    total = 0
+    print(frame.shape)
     while was_read:
-        if frame_type == 'I':
-            print(i)
-            # cv2.imwrite(f'images/{i}.png', frame)
-            was_read, frame, vectors, frame_type, _ = vectors_cap.read()
-            i += 1
-            continue
-        # mvs = vectors[:, 3:7]
-        # print(mvs)
-        # if len(mvs):
-            # frame = BlockMatching.draw_motion_vectors(frame, mvs)
-        # cv2.imwrite(f'images/{i}.png', frame)
+        # if frame_type == 'I':
+        #     print('i frame')
+        #     was_read, frame, vectors, frame_type, _ = vectors_cap.read()
+        #     continue
+        mvs = vectors[:, 3:7]
+        # sols = Formula.calculate(mvs, camera_matrix, 'y', decimal_places=2, interval=(-5, 5), remove_zeros=True)
+        # Formula.graph_solutions(sols, '', bars_count=5, show=True)
         was_read, frame, vectors, frame_type, _ = vectors_cap.read()
-
+        frame = BlockMatching.draw_motion_vectors(frame, mvs)
+        cv2.imshow('', frame)
+        cv2.waitKey()
+        # if len(sols):
+        #     max_sol = abs(max(sols.items(), key=lambda x: x[1])[0])
+        #     total += max_sol
         i += 1
+        print(i, total)
