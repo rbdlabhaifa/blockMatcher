5f63ba7c4200f13a383cb492d86887a3e1d4f694
RAni <ranmn123@gmail.com>
2022-10-24 18:19:56 +0300

    Added drone video.

--- main.py
+++ main.py
@@ -1,86 +1,37 @@
-import cv2
-import numpy as np
-import os
-from block_matching import BlockMatching
-from formula import *
+import time, cv2
+from threading import Thread
+from djitellopy import Tello
 
+tello = Tello()
 
-def view_vectors(path_to_data, suffix='.png'):
-    if suffix == '.jpg':
-        with open(path_to_data + '.csv', 'r') as file:
-            rots = []
-            for i in file:
-                rots.append(float(i))
-        angles = [abs(rots[i] - rots[i - 1]) for i in range(1, len(rots))]
-        motion_vectors = BlockMatching.extract_motion_data(path_to_data + '.mp4')
-    else:
-        frames = [f'{path_to_data}/{i}' for i in
-                  sorted(os.listdir(path_to_data), key=lambda x: int(x.replace(suffix, '')))]
-        motion_vectors = BlockMatching.get_ffmpeg_motion_vectors(frames)
-    for i, vectors in enumerate(motion_vectors):
-        if suffix == '.png':
-            if i % 2 == 1:
-                continue
-            print('angle=', 0.1 * ((i // 2) + 1))
-        else:
-            print('angle=', angles[i])
-        base_image = cv2.imread(path_to_data + '/0' + suffix)
-        base_image = BlockMatching.draw_motion_vectors(base_image, vectors)
-        cv2.imshow('', base_image)
-        input('enter something')
+tello.connect()
 
+keepRecording = True
+tello.streamon()
+frame_read = tello.get_frame_read()
 
-if __name__ == '__main__':
-    from mvextractor.videocap import VideoCap
+def videoRecorder():
+    framenum = 0
+    while keepRecording:
+        cv2.imwrite(f'/home/rani/PycharmProjects/blockMatcher/data/drone/3/{framenum}.png', frame_read.frame)
+        framenum += 1
+        time.sleep(1 / 30)
 
-    # 640 x 480
-    # camera_matrix = np.array([[647.81841563, 0, 335.8814632],
-    #                           [0, 645.9438274, 225.99776891001],
-    #                           [0, 0, 1]])
-    # 1280 x 720
-    # camera_matrix = np.array([[1374.469951742393, 0, 638.379203340978],
-    #                           [0, 1380.753021831714, 374.04638859868663],
-    #                           [0, 0, 1]])
-    # 960 x 720
-    camera_matrix = np.array([
-        [907.15425425, 0, 478.97775485],
-        [0, 907.55348533, 346.90456115],
-        [0, 0, 1]
-    ])
-    vectors_cap = VideoCap()
-    vectors_cap.open('/home/rani/PycharmProjects/blockMatcher/data/drone/1.mp4')
-    was_read, frame, vectors, frame_type, _ = vectors_cap.read()
-    i = 0
-    total = 0
-    print(frame.shape)
-    all_sols = {}
-    x, y = [], []
-    while was_read:
-        # if frame_type == 'I':
-        #     print('i frame')
-        #     was_read, frame, vectors, frame_type, _ = vectors_cap.read()
-        #     continue
-        mvs = vectors[:, 3:7]
-        sols = Formula.calculate(mvs, camera_matrix, 'y', decimal_places=2, interval=(-2, 2), remove_zeros=True)
-        # Formula.graph_solutions(sols, '', bars_count=5, show=True)
-        was_read, frame, vectors, frame_type, _ = vectors_cap.read()
-        # frame = BlockMatching.draw_motion_vectors(frame, mvs)
-        # cv2.imshow('', frame)
-        # cv2.waitKey()
-        if len(sols):
-            x.append(i)
-            max_sol = abs(max(sols.items(), key=lambda x: x[1])[0])
-            y.append(max_sol)
-            total += max_sol
-            all_sols[i] = total
+recorder = Thread(target=videoRecorder)
 
-        i += 1
-        print(i, total)
-    print(all_sols)
-    import matplotlib.pyplot as plt
+# tello.streamon()
+tello.takeoff()
+# time.sleep(3)
+# recorder.start()
 
-    plt.plot(x, y)
-    # plt.title('Unemployment Rate Vs Year')
-    # plt.xlabel('sum of angles')
-    # plt.ylabel('Unemployment Rate')
-    plt.show()
+tello.rotate_clockwise(20)
+tello.land()
+
+keepRecording = False
+recorder.join()
+
+# 360 error of 1 deg
+# 5 error of 0.45 deg
+# 10 error of 1.3 deg
+# 15 error of 1.3 deg
+# 20 error of 1.4 deg
